e355cd4117ac31ccb7aec2342d1cf27a7c4fa40c
Diego Castro <spinolacastro@gmail.com>
2014-09-19 20:20:02 -0700

    first

--- worker/recipes/deploy.rb
+++ worker/recipes/deploy.rb
@@ -16,6 +16,9 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  current_dir = ::File.join(deploy[:deploy_to], 'current')
+  webapp_dir = ::File.join(node['worker']['basedir'], deploy[:document_root].blank? ? application : deploy[:document_root])
+
   link webapp_dir do
     to current_dir
     action :create
