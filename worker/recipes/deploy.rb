1384753faa3107de2f58a00f0f27e6441ed136e3
Diego Castro <spinolacastro@gmail.com>
2014-09-19 17:21:50 -0700

    first

--- worker/recipes/deploy.rb
+++ worker/recipes/deploy.rb
@@ -3,5 +3,5 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
  	if deploy[:application_type] != 'php'
   	Chef::Log.debug("Skipping php::configure application #{application} as it is not an PHP app")
- 	next
+ 	end
 end
