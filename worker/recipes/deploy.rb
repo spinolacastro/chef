dd3bfcd491d34ed8fbaf2812c1604d8da383a3d5
Diego Castro <spinolacastro@gmail.com>
2014-09-19 17:18:08 -0700

    first

---
+++ worker/recipes/deploy.rb
@@ -0,0 +1,7 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+ 	if deploy[:application_type] != 'php'
+  	Chef::Log.debug("Skipping php::configure application #{application} as it is not an PHP app")
+ 	next
+end
